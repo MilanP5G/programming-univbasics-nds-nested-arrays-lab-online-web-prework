380dd56e64fa6f6ef1f893dadf703bca4c73de56
Milan Parmar <milan_parmar@hotmail.co.uk>
2019-11-23 12:45:19 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -47,9 +47,9 @@ end
 def matrix_update(matrix, row, column, new_value)
   
     
-  assembled_matrix[0][2] = "Tiny Ewell"
+   assembled_matrix[0][2] = "Tiny Ewell"
   
-  return assembled_matrix
+   assembled_matrix
   
   
   # Update the matrix location at row and column to have the value of new_value
